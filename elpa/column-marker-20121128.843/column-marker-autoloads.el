1a8208da3f4b41eb9c90ef74aebf3ca1b6d77758
Brandon DeRosier <btd@cheesekeg.com>
2014-04-29 10:18:57 -0400

    Many changes for working with Javascript

---
+++ elpa/column-marker-20121128.843/column-marker-autoloads.el
@@ -0,0 +1,27 @@
+;;; column-marker-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "column-marker" "column-marker.el" (21317 43793
+;;;;;;  401912 750000))
+;;; Generated autoloads from column-marker.el
+
+(autoload 'column-marker-1 "column-marker" "\
+Highlight a column." t)
+
+;;;***
+
+;;;### (autoloads nil nil ("column-marker-pkg.el") (21317 43793 505450
+;;;;;;  578000))
+
+;;;***
+
+(provide 'column-marker-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; column-marker-autoloads.el ends here
